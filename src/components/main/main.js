e63127edae9470345c69a2ee1abd12d288dd39ef
Hans Krohn <hanskrohn70@gmail.com>
2020-07-17 00:04:51 -0400

    fixed typo

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -30,7 +30,7 @@ const Main = (props) => {
             Since arriving in Florida, I have been amazed with the amount of lizards that populate this region. As I worked on my computer, I frequently found myself
             keeping a mental track of the lizards that skittered across my window. I even witnessed how they hunt flies! I began wanting to keep track of
             quantitave and qualitative data I saw. At first, I thought of using exel, but with my limited knowledge in exel that seemed like a daunting task.
-            Thats when I decided to begin developing this Web App. The goal of this app is to allow user to create easily create a visual representation of data.
+            Thats when I decided to begin developing this Web App. The goal of this app is to allow user to easily create a visual representation of data.
           </p>
         </div>
       </div>
